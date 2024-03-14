2e11c103b37b44a20d22bf7d14939c80e1bb142d
=swalih <=sibiyan9030@gmail.com>
2024-03-14 23:45:22 +0530

    first

---
+++ build/precache-manifest.b7aa9d0a4a4e578066c2e7ca54c836a5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6973228fce546f4b082a51940f3152b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e43420f6520a3afa594",
+    "url": "/static/js/2.eca8b71d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.eca8b71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b613b215f7c639e2b16e",
+    "url": "/static/js/main.6469c6c8.chunk.js"
+  },
+  {
+    "revision": "5dcc4a2d51d1022f96e5",
+    "url": "/static/js/runtime-main.edb82d6c.js"
+  },
+  {
+    "revision": "d71efe41c58e81307940065297d75199",
+    "url": "/static/media/laam_app1.d71efe41.png"
+  },
+  {
+    "revision": "4beafb92e30b7ef09ff7ee769317a0bf",
+    "url": "/static/media/mern_blog.4beafb92.png"
+  },
+  {
+    "revision": "072e780504a1f540e84e8b7a8b6b33c3",
+    "url": "/static/media/nlp_app.072e7805.png"
+  },
+  {
+    "revision": "a65287f04b7caf76381519707f0045e8",
+    "url": "/static/media/profile_3.a65287f0.jpg"
+  },
+  {
+    "revision": "21e1b60ba430ae424b3904703a42ed6f",
+    "url": "/static/media/real_estate.21e1b60b.png"
+  },
+  {
+    "revision": "729a19b39bbee1e843d3c7450f9d8e9c",
+    "url": "/static/media/svg-14.729a19b3.svg"
+  },
+  {
+    "revision": "5118199275350eb1346478e5b3039e0f",
+    "url": "/static/media/svg-15.51181992.svg"
+  },
+  {
+    "revision": "0b3df8a7073e7236c2d6b4e629397700",
+    "url": "/static/media/svg-16.0b3df8a7.svg"
+  }
+]);
